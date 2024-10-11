87c4aac632a6aecd7e14dc0282c3ef71c168a676
Favour James <Favourjames061@gmail.com>
2024-10-11 01:30:30 +0000

    BAsic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Basic annotaions - Floor"""
+
+
+def floor(n: float) -> floor:
+    """type-annotated function floor which takes a float n
+    as argument and returns the floor of the float
+    """
+    return floor(n)
